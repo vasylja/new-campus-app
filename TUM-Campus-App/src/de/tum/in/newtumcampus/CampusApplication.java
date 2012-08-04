24f0473f7827c8d9a0fc5f752e9cb923f9e9dd39
Florian Schulz <florian.schulz@in.tum.de>
2012-08-04 15:14:56 +0200

    CampusApplication is now commented

--- TUM-Campus-App/src/de/tum/in/newtumcampus/CampusApplication.java
+++ TUM-Campus-App/src/de/tum/in/newtumcampus/CampusApplication.java
@@ -4,11 +4,19 @@ import org.acra.*;
 import org.acra.annotation.*;
 import android.app.Application;
 
+
+/**
+ * @author Florian Schulz
+ * @source http://code.google.com/p/acra/wiki/AdvancedUsage
+ * CampusApplication solves an global exception handler,
+ * socketTimeout is increased for G2-Networks,
+ * and a TOAST is shown to inform the users.
+ */
 @ReportsCrashes(formKey = "dFZGRlBhUVgyd292TlpOVk44RnBid0E6MQ",
 mode = ReportingInteractionMode.TOAST,
 resToastText = R.string.handle_error,
 socketTimeout = 8000)
-public class CampusApplication extends Application {
+public class CampusApplication extends Application {	
 	@Override
 	public void onCreate(){
 		// initialization of ACRA ReportCrash
