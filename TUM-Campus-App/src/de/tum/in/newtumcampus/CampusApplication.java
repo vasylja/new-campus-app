3fe90ed4d4e46eaf2889da8fa0e43612a838306f
Florian Schulz <florian.schulz@in.tum.de>
2012-08-13 20:03:57 +0200

    Aenderung im ExceptionHandler

--- TUM-Campus-App/src/de/tum/in/newtumcampus/CampusApplication.java
+++ TUM-Campus-App/src/de/tum/in/newtumcampus/CampusApplication.java
@@ -13,9 +13,17 @@ import android.app.Application;
  * and a TOAST is shown to inform the users.
  */
 @ReportsCrashes(formKey = "dFZGRlBhUVgyd292TlpOVk44RnBid0E6MQ",
-mode = ReportingInteractionMode.TOAST,
-resToastText = R.string.handle_error,
-socketTimeout = 8000)
+				mode = ReportingInteractionMode.NOTIFICATION,
+				resNotifTickerText = R.string.crash_notif_ticker_text,
+				resNotifTitle = R.string.crash_notif_title,
+				resNotifText = R.string.crash_notif_text,
+				resNotifIcon = android.R.drawable.stat_notify_error, // optional. default is a warning sign
+				resDialogText = R.string.crash_dialog_text,
+				resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+				resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+				resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+				resDialogOkToast = R.string.crash_dialog_ok_toast, // optional. displays a Toast message when the user accepts to send a report.
+				socketTimeout = 8000)
 public class CampusApplication extends Application {	
 	@Override
 	public void onCreate(){
